39691d806bb3a6777d0e80e1decafc0737d6bee4
elena1989 <elena.shilova89@gmail.com>
2022-10-11 15:27:49 +0300

    тест пройден

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
         System.out.println(bean.getMessage());
+        System.out.println(bean==bean1);
+        System.out.println(cat1==cat2);
+
     }
 }
